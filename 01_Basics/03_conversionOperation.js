7d6a76493bf545875379fad66c1f9fd8fb7f6b3e
TejasPatil75 <tejasbpatil31may@gmail.com>
2024-09-28 14:05:49 +0000

    conversion and operations

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,65 @@
+let score = "tejas"
+
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+
+// console.log(typeof (valueInNumber))
+// console.log(valueInNumber);
+
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1 ; false -> 0
+
+let isLoggedIn = "tejas"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 -> true ; 0 -> false
+// "" -> false ; "tejas" -> true 
+
+let someNum = 33
+
+let strNum = String(someNum)
+// console.log(strNum)
+// console.log(typeof (strNum))
+
+// **************************** Operations ****************************
+
+let value = 3 
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "tejas"
+let str2 = " patil"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12 
+// console.log("1" + 2 + 2); // 122 
+// console.log(1 + 2 + "2"); // 32
+
+// console.log( (3+4)*5 %3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1 , num2 , num3 
+
+num1 = num2 = num3 = 2 + 2 
+
+let gameCounter = 100 
+++gameCounter;
+console.log(gameCounter);
+
+
