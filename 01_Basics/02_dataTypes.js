d567c238d1206f78665de46119a0c56dd16cd0a6
TejasPatil75 <tejasbpatil31may@gmail.com>
2024-09-28 11:41:07 +0000

    new file 2

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" ; // treat all JS code as newer version
+
+// alert (3+3) // we are using node js not browser
+
+console.log(3+3) // code readability should be high
+
+console.log("TEJAS")
+
+let name = "tejas"
+let age = 20 
+let isLoggedIn = false
+let state; 
+
+// number -> 2 ^ 53 
+// bigint 
+// string -> ""
+// boolean -> true / false 
+// null -> standalone 
+// undefined ->
+// symbol -> unique 
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
