c3a20c4fc01d8d07d8de2de1f1adac01df77ee0e
TejasPatil75 <tejasbpatil31may@gmail.com>
2024-10-01 18:55:31 +0000

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+
+let idx = 0 ;
+// while(idx <= 10)
+// {
+//     console.log(`Value of index is ${idx}`);
+//     idx += 2 
+// }
+
+let myArr = ['flash' , "batman" , "superman"]
+
+let i = 0 ;
+while(i < myArr.length)
+{
+    // console.log(myArr[i]);
+    i++
+}
+
+let score = 11 
+
+do{
+    console.log(`Score is ${score}`);
+    score ++
+}
+while(score <= 10)
